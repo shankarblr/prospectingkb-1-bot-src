13e32a9df321d06a146541967aeca443a1c529f6
Shankar Subramanyam <shankarblr@hotmail.com>
2020-01-11 15:44:30 -0600

    Inital Commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=prospectingkb-1-bot-Web-Deploy.pubxml -p:Password=ZuEF0d61iSlhqtcKHZEF15hkftcsqCvZBmK9qFmlRKp2llqDB29KY4b1xxib
+
